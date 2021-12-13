0b94ae7e3c42262a34bc870624a69abbf13d40b9
Grzegorz Panek <grzegorzpnk@gmail.com>
2021-12-14 00:09:32 +0100

    multiple vars declaring

--- main.go
+++ main.go
@@ -4,6 +4,19 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("test1")
+	//this is the code how to define vars in golang
+
+	var1 := "test1"
+	fmt.Println(var1)
+
+	var var2 string
+	var2 = "test2"
+	fmt.Println(var2)
+
+	var var3 string = "test3"
+	fmt.Println(var3)
+
+	var var4 = "test4"
+	fmt.Println(var4)
 
 }
